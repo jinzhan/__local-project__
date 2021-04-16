060e4dcbe3124f77eebf998b652e36bf12ad78cf
jinzhan <jinzhan@baidu.com>
2021-04-16 16:27:42 +0800

    init

---
+++ src/pages/index/index.js
@@ -0,0 +1,16 @@
+/**
+ * @file index
+ */
+
+import App from '@/lib/App';
+import AppComponent from './containers/app';
+
+App(AppComponent, '#app');
+
+const a = new Promise;
+const b = new Map;
+document.write({a, b});
+
+// console.log(a);
+
+// console.log('haha');
